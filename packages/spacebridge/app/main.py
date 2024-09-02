9178c27b58ed693ea2c43b1962eb67d127e8912f
hongymagic <302730+hongymagic@users.noreply.github.com>
2024-09-02 19:14:14 +1000

    feat: initial uv based monorepo for python!

---
+++ packages/spacebridge/app/main.py
@@ -0,0 +1,15 @@
+from fastapi import Depends, FastAPI
+
+from .dependencies import get_query_token, get_token_header
+from .routers import items, users
+
+app = FastAPI(dependencies=[Depends(get_query_token)])
+
+
+app.include_router(users.router)
+app.include_router(items.router)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello Bigger Applications!"}
